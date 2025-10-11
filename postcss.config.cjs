735c8e93ef3e2dc8b9e1cc91fb8f01f1530108c8
Andre1qaz <andresaragih932@gmail.com>
2025-10-11 23:01:27 +0700

    update

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,11 @@
-// eslint-disable-next-line no-undef
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 }
